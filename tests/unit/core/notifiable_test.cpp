04e487cb8ffd40c285d1c80041dc2261dc2b5e74
Maksim <maksimshchavelev@gmail.com>
2026-02-09 17:28:53 +0300

    feat: add Notifiable class

--- tests/unit/core/notifiable_test.cpp
+++ tests/unit/core/notifiable_test.cpp
@@ -1,5 +1,55 @@
+#include <core/Notifiable.hpp>
 #include <gtest/gtest.h>
 
-TEST(StreakServiceTest, GetAndSet) {
-    ASSERT_TRUE(true);
+class MyNotifiable final : public core::Notifiable {
+    SIGNAL(updated)
+    SIGNAL(updated_value, int value)
+
+public:
+    int value() const noexcept {
+        return m_value;
+    }
+
+    void update() {
+        dispatch(updated{});
+    }
+
+    void update(int value) {
+        m_value = value;
+        dispatch(updated_value{m_value});
+    }
+
+  private:
+    int m_value{0};
+};
+
+
+TEST(NotifiableTest, NoArgsSignal) {
+    MyNotifiable n;
+    bool called{false};
+
+    n.connect<MyNotifiable::updated>([&called]([[maybe_unused]] const MyNotifiable::updated& signal){
+        called = true;
+    });
+
+    n.update();
+
+    ASSERT_TRUE(called);
+}
+
+
+TEST(NotifiableTest, OneArgumentSignal) {
+    MyNotifiable n;
+    bool called{false};
+    int got_value{0};
+
+    n.connect<MyNotifiable::updated_value>([&called, &got_value](const MyNotifiable::updated_value& signal){
+        called = true;
+        got_value = signal.value;
+    });
+
+    n.update(45);
+
+    ASSERT_TRUE(called);
+    ASSERT_EQ(got_value, 45);
 }
