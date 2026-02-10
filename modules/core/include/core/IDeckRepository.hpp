f8dd52a93a0bf20f324ef31e3e26507073c797b1
Maksim <maksimshchavelev@gmail.com>
2026-02-10 17:07:45 +0300

    move IDeckRepository to core module

---
+++ modules/core/include/core/IDeckRepository.hpp
@@ -0,0 +1,48 @@
+// Copyright 2025 Maksim Shchavelev <maksimshchavelev@gmail.com>
+
+#pragma once
+
+#include "Card.hpp"  // for Card
+#include "Deck.hpp"  // for Deck
+#include <QDateTime> // for QDateTime
+#include <QString>   // for QString
+#include <QVector>   // for QVector
+#include <expected>  // for std::expected
+
+namespace core {
+
+struct CardBackUpdate {
+    int       cardId;
+    QString   back;
+    QDateTime updated_at;
+};
+
+/**
+ * @brief Repository interface for deck/card persistence.
+ *
+ * Implementations (e.g. SqlDeckRepository) provide concrete storage logic.
+ */
+class IDeckRepository {
+  public:
+    virtual ~IDeckRepository() = default;
+
+    virtual std::expected<QVector<core::DeckSummary>, QString> get_deck_summaries(const QDateTime& now) = 0;
+    virtual std::expected<QVector<core::Card>, QString>        get_cards(int deckId) = 0;
+    virtual std::expected<core::Deck, QString>                 get_deck(int deckId) = 0;
+
+    virtual std::expected<int, QString>  create_deck(const core::Deck& deck) = 0;
+    virtual std::expected<void, QString> update_deck(const core::Deck& deck) = 0;
+    virtual std::expected<void, QString> delete_deck(int deckId) = 0;
+    virtual std::expected<bool, QString> is_deck_exists(const QString& deck_name) = 0;
+    virtual std::expected<int, QString>  deck_id(const QString& deck_name) = 0;
+
+    virtual std::expected<void, QString>       update_card(const core::Card& card) = 0;
+    virtual std::expected<void, QString>       insert_cards_batch(int deckId, const QVector<core::Card>& cards) = 0;
+    virtual std::expected<void, QString>       update_card_backs_batch(const QVector<CardBackUpdate>& cards) = 0;
+    virtual std::expected<void, QString>       insert_card(int deckId, const core::Card& card) = 0;
+    virtual std::expected<bool, QString>       is_card_exists(int deckId, const QString& card_front) = 0;
+    virtual std::expected<core::Card, QString> get_card(int cardId) = 0;
+    virtual std::expected<void, QString>       remove_card(int cardId) = 0;
+};
+
+} // namespace core
