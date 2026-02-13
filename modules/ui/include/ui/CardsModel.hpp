105875c178716a92435ee7ac95b55bbc9536a89f
Maksim <maksimshchavelev@gmail.com>
2026-02-13 16:01:04 +0300

    add CardsModel to ui

---
+++ modules/ui/include/ui/CardsModel.hpp
@@ -0,0 +1,46 @@
+// Copyright 2025 Maksim Shchavelev <maksimshchavelev@gmail.com>
+
+#pragma once
+
+#include <QAbstractListModel>    // for QAbstractListModel
+#include <core/IDeckService.hpp> // for core::IDeckService
+
+namespace io {
+
+class CardsModel final : public QAbstractListModel {
+    Q_OBJECT
+
+  public:
+    Q_PROPERTY(int cardsCount READ cards_count NOTIFY updated FINAL);
+
+    CardsModel(core::IDeckService& service);
+
+    enum CardRoles { FrontRole = Qt::UserRole + 1, BackRole, IdRole };
+
+    /**
+     * @brief Set current deck. The cards will be loaded specifically for this deck.
+     * @param deckId ID of deck
+     */
+    Q_INVOKABLE void setDeck(int deckId);
+
+    int                    rowCount(const QModelIndex& parent = QModelIndex()) const override;
+    QVariant               data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
+    QHash<int, QByteArray> roleNames() const override;
+
+  signals:
+    void updated();
+
+  private:
+    core::IDeckService& m_deck_service;
+    QVector<core::Card> m_cards;
+    int                 m_last_deck_id{0};
+
+    /**
+     * @brief Get cards count
+     * @return Count of the cards
+     * @note First, call the `load` method to load information about the deck.
+     */
+    int cards_count() const noexcept;
+};
+
+} // namespace io
---
+++ modules/ui/src/CardsModel.cpp
@@ -0,0 +1,65 @@
+// Copyright 2025 Maksim Shchavelev <maksimshchavelev@gmail.com>
+
+#include "ui/CardsModel.hpp" // for header
+
+namespace io {
+
+CardsModel::CardsModel(core::IDeckService& service) : m_deck_service(service) {
+    m_deck_service.connect<core::IDeckService::cards_updated>([this](const core::IDeckService::cards_updated& e) {
+        setDeck(m_last_deck_id); // reload
+    });
+}
+
+
+int CardsModel::rowCount(const QModelIndex& parent) const {
+    return m_cards.size();
+}
+
+
+QVariant CardsModel::data(const QModelIndex& index, int role) const {
+    if (!index.isValid() || index.row() < 0 || index.row() >= m_cards.size()) {
+        return QVariant();
+    }
+
+    const core::Card& card = m_cards.at(index.row());
+
+    switch (role) {
+    case FrontRole:
+        return card.front;
+    case BackRole:
+        return card.back;
+    case IdRole:
+        return card.id;
+    default:
+        return QVariant();
+    }
+}
+
+
+QHash<int, QByteArray> CardsModel::roleNames() const {
+    static QHash<int, QByteArray> roles{{FrontRole, "front"}, {BackRole, "back"}, {IdRole, "id"}};
+
+    return roles;
+}
+
+
+void CardsModel::setDeck(int deckId) {
+    m_last_deck_id = deckId;
+
+    beginResetModel();
+    auto res = m_deck_service.cards(m_last_deck_id);
+
+    if (res.has_value()) {
+        m_cards = std::move(res.value());
+    }
+
+    endResetModel();
+    emit updated();
+}
+
+
+int CardsModel::cards_count() const noexcept {
+    return m_cards.size();
+}
+
+} // namespace io
