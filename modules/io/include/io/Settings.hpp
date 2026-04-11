d0cf37be2fa805291ac5a81289da974edc1f419a
Maksim <maksimshchavelev@gmail.com>
2026-04-11 14:19:24 +0300

    add settings implementation

---
+++ modules/io/include/io/Settings.hpp
@@ -0,0 +1,31 @@
+// Copyright 2025 Maksim Shchavelev <maksimshchavelev@gmail.com>
+
+#pragma once
+
+#include <QSettings>                   // for QSettings
+#include <core/settings/ISettings.hpp> // for core::ISettings
+
+namespace io {
+
+class Settings : public core::ISettings {
+  public:
+    /// @param path Path to settings file
+    Settings();
+
+    /// @copydoc core::ISettings::learning_rate
+    float learning_rate() const override;
+
+    /// @copydoc core::ISettings::set_learning_rate
+    virtual std::expected<void, core::SettingsError> set_learning_rate(float rate) override;
+
+    /// @copydoc core::ISettings::max_interval
+    virtual int max_interval() const override;
+
+    /// @copydoc core::ISettings::set_max_interval
+    virtual std::expected<void, core::SettingsError> set_max_interval(int interval) override;
+
+  private:
+    QSettings m_settings;
+};
+
+} // namespace io
---
+++ modules/io/src/Settings.cpp
@@ -0,0 +1,37 @@
+// Copyright 2025 Maksim Shchavelev <maksimshchavelev@gmail.com>
+
+#include "io/Settings.hpp" // for header
+
+namespace io {
+
+Settings::Settings() : m_settings("revise", "revise") {}
+
+float Settings::learning_rate() const {
+    return m_settings.value("algorithm_learning_rate", 2.5f).toFloat();
+}
+
+std::expected<void, core::SettingsError> Settings::set_learning_rate(float rate) {
+    if (rate < 1.0f || rate > 5.0f) {
+        return std::unexpected(core::SettingsError{.kind = core::SettingsError::Kind::OutOfBounds,
+                                                   .message = QStringLiteral("rate out of bounds")});
+    }
+
+    m_settings.setValue("algorithm_learning_rate", rate);
+    return {};
+}
+
+int Settings::max_interval() const {
+    return m_settings.value("algorithm_max_interval", 14).toInt();
+}
+
+std::expected<void, core::SettingsError> Settings::set_max_interval(int interval) {
+    if (interval < 1 || interval > 365) {
+        return std::unexpected(core::SettingsError{.kind = core::SettingsError::Kind::OutOfBounds,
+                                                   .message = QStringLiteral("rate out of bounds")});
+    }
+
+    m_settings.setValue("algorithm_max_interval", interval);
+    return {};
+}
+
+} // namespace io
