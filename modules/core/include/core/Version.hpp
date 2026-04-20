096cfebf7447153be64c5653c9622911e7a3d240
Maksim <maksimshchavelev@gmail.com>
2026-04-20 16:17:10 +0300

    bump version

--- modules/core/include/core/Version.hpp
+++ modules/core/include/core/Version.hpp
@@ -7,6 +7,6 @@ namespace core {
 constexpr int REVISE_DB_SCHEMA_VERSION = 2;
 constexpr int REVISE_VERSION_MAJOR = 1;
 constexpr int REVISE_VERSION_MINOR = 3;
-constexpr int REVISE_VERSION_PATCH = 0;
+constexpr int REVISE_VERSION_PATCH = 3;
 
 }
