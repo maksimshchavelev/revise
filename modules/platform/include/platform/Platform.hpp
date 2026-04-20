72772af58f579f11c9cfa8aa1440dd472ae9e353
Maksim <maksimshchavelev@gmail.com>
2026-04-20 15:26:35 +0300

    add android_sdk_version()

--- modules/platform/include/platform/Platform.hpp
+++ modules/platform/include/platform/Platform.hpp
@@ -2,18 +2,34 @@
 
 #pragma once
 
+#include <QtGlobal> // for Qt macro
+#include <optional> // for std::optional
+
+#ifdef Q_OS_ANDROID
+#include <QJniObject> // for QJniObject
+#endif
+
 namespace platform {
 
 /// @breif A static class with helper constants for each platform
 class Platform {
   public:
     /// @brief Returns true if the target platform supports multiple windows for a single application
-    static consteval bool supports_multiple_windows() {
+    static consteval bool supports_multiple_windows() noexcept {
 #ifdef Q_OS_ANDROID
         return false;
 #else
         return true;
 #endif
+    }
+
+    /// @brief Returns Android SDK version. Returns std::nullptr on other platforms
+    static std::optional<int> android_sdk_version() noexcept {
+#ifdef Q_OS_ANDROID
+        return QJniObject::getStaticField<jint>("android/os/Build$VERSION", "SDK_INT");
+#endif
+
+        return std::nullopt;
     }
 };
 
