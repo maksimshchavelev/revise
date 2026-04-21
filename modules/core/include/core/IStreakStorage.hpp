81f7946e0d4986380e7acf8c485595ba43aaaab8
Maksim <maksimshchavelev@gmail.com>
2026-04-21 18:27:27 +0300

    add errors to IStreakStorage

--- modules/core/include/core/IStreakStorage.hpp
+++ modules/core/include/core/IStreakStorage.hpp
@@ -13,21 +13,45 @@ namespace core {
  */
 class IStreakStorage {
   public:
+    /// @brief Describes streak storage errors
+    struct Error {
+        enum class Kind {
+            Unavailable,  ///< Storage unavailable
+            MissingValue, ///< Value doesn't exist
+            InternalError ///< Internal error
+        };
+
+        Kind    kind;
+        QString message;
+
+        static Error unavailable(QString message) {
+            return Error{.kind = Kind::Unavailable, .message = std::move(message)};
+        }
+
+        static Error missing_value(QString message) {
+            return Error{.kind = Kind::MissingValue, .message = std::move(message)};
+        }
+
+        static Error internal_error(QString message) {
+            return Error{.kind = Kind::InternalError, .message = std::move(message)};
+        }
+    };
+
+    using ErrorKind = Error::Kind;
+    template <typename T> using Result = std::expected<T, Error>;
+
     virtual ~IStreakStorage() = default;
 
     /**
      * @brief Saves the current streak
      * @param streak The `core::Streak` structure describing a streak
-     * @return `std::expected <void, QString>` with `void` if success, otherwise with error description
      */
-    virtual std::expected<void, QString> save(const Streak streak) = 0;
+    virtual Result<void> save(const Streak streak) = 0;
 
     /**
      * @brief Reads the current streak
-     * @return `std::expected <Streak, QString>` with `core::Streak` structure describing a streak if success, otherwise
-     * with error description
      */
-    virtual std::expected<Streak, QString> read() = 0;
+    virtual Result<Streak> read() = 0;
 };
 
 } // namespace core
