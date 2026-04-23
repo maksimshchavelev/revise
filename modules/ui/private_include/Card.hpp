141c3ec7fa4642da8b6124600a42607214848b79
Maksim <maksimshchavelev@gmail.com>
2026-04-23 18:24:09 +0300

    add CardWrapper

--- modules/ui/private_include/Card.hpp
+++ modules/ui/private_include/Card.hpp
@@ -5,48 +5,65 @@
 #include <QObject>                            // for Q_GADGET
 #include <QtQmlIntegration/qqmlintegration.h> // for QML_ELEMENT
 #include <core/Card.hpp>                      // for core::Card
+#include <core/Property.hpp>                  // for PROPERTY macro for getters and setters
 
 namespace ui {
 
-/**
- * @brief Wrapper over core::Card, visible from QML
- */
-struct Card final : core::Card {
+/// @brief Wrapper over core::Card, visible from QML
+struct CardWrapper {
     Q_GADGET
     QML_ELEMENT
 
-    Q_PROPERTY(int id MEMBER id FINAL)
-    Q_PROPERTY(int global_id MEMBER global_id FINAL)
-    Q_PROPERTY(int deck_id MEMBER deck_id FINAL)
-    Q_PROPERTY(int state MEMBER state FINAL)
-    Q_PROPERTY(int incorrect_streak MEMBER incorrect_streak FINAL)
-    Q_PROPERTY(int time_limit MEMBER time_limit FINAL)
-    Q_PROPERTY(float difficulty MEMBER difficulty FINAL)
-    Q_PROPERTY(QDateTime next_review MEMBER next_review FINAL)
-    Q_PROPERTY(QDateTime created_at MEMBER created_at FINAL)
-    Q_PROPERTY(QDateTime updated_at MEMBER updated_at FINAL)
-    Q_PROPERTY(QString front MEMBER front FINAL)
-    Q_PROPERTY(QString back MEMBER back FINAL)
+    Q_PROPERTY(int id READ id WRITE set_id NOTIFY updated)
+    Q_PROPERTY(int global_id READ global_id WRITE set_global_id NOTIFY updated)
+    Q_PROPERTY(int deck_id READ deck_id WRITE set_deck_id NOTIFY updated)
+    Q_PROPERTY(int incorrect_streak READ incorrect_streak WRITE set_incorrect_streak NOTIFY updated)
+    Q_PROPERTY(int interval READ interval WRITE set_interval NOTIFY updated)
+    Q_PROPERTY(float difficulty READ difficulty WRITE set_difficulty NOTIFY updated)
+    Q_PROPERTY(float time_limit READ time_limit WRITE set_time_limit NOTIFY updated)
+    Q_PROPERTY(QDateTime next_review READ next_review WRITE set_next_review NOTIFY updated)
+    Q_PROPERTY(QDateTime created_at READ created_at WRITE set_created_at NOTIFY updated)
+    Q_PROPERTY(QDateTime updated_at READ updated_at WRITE set_updated_at NOTIFY updated)
+    Q_PROPERTY(QString front READ front WRITE set_front NOTIFY updated)
+    Q_PROPERTY(QString back READ back WRITE set_back NOTIFY updated)
 
   public:
-    Q_INVOKABLE Card() = default;
+    CardWrapper() = default;
 
-    Card(const core::Card& other) {
-        *this = other;
-    }
+    CardWrapper(const core::Card& card) : m_card{card} {}
+    CardWrapper(core::Card&& card) : m_card{std::move(card)} {}
 
-    template <typename OtherCard>
-        requires std::is_base_of_v<core::Card, std::remove_reference_t<OtherCard>>
-    Card& operator=(OtherCard&& other) {
-        core::Card::operator=(std::forward<OtherCard>(other));
+    CardWrapper(const CardWrapper&) = default;
+    CardWrapper(CardWrapper&&) = default;
+    CardWrapper& operator=(const CardWrapper&) = default;
+    CardWrapper& operator=(CardWrapper&&) = default;
+
+    CardWrapper& operator=(const core::Card& card) {
+        m_card = card;
         return *this;
     }
 
-    template <typename OtherCard>
-        requires std::is_base_of_v<core::Card, std::remove_reference_t<OtherCard>>
-    bool operator==(const OtherCard& other) const noexcept {
-        return core::Card::operator==(other);
+    CardWrapper& operator=(core::Card&& card) {
+        m_card = std::move(card);
+        return *this;
     }
+
+    PROPERTY(id, m_card.id);
+    PROPERTY(global_id, m_card.global_id)
+    PROPERTY(deck_id, m_card.deck_id)
+    PROPERTY(difficulty, m_card.difficulty)
+    PROPERTY(state, m_card.state)
+    PROPERTY(incorrect_streak, m_card.incorrect_streak)
+    PROPERTY(interval, m_card.interval)
+    PROPERTY(time_limit, m_card.time_limit)
+    PROPERTY(next_review, m_card.next_review)
+    PROPERTY(created_at, m_card.created_at)
+    PROPERTY(updated_at, m_card.updated_at)
+    PROPERTY(front, m_card.front)
+    PROPERTY(back, m_card.back)
+
+  private:
+    core::Card m_card;
 };
 
 } // namespace ui
