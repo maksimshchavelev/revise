eb4bb06889f9472acb5f74a3d5842dedaede7fb6
Maksim <maksimshchavelev@gmail.com>
2026-04-24 17:43:19 +0300

    returning modified decks and cards

--- modules/core/include/core/storage/IDeckStorage.hpp
+++ modules/core/include/core/storage/IDeckStorage.hpp
@@ -36,71 +36,77 @@ class IDeckStorage {
     /**
      * @brief Create new decks from a vector
      * @param decks Vector of deck objects (`core::Deck`)
+     * @return Created decks
      */
-    virtual Result<void> create_decks(const QVector<Deck>& decks) = 0;
+    virtual Result<QVector<Deck>> create_decks(const QVector<Deck>& decks) = 0;
 
     /**
      * @brief Update existing decks from a vector
      * @param decks Vector of deck objects (`core::Deck`)
+     * @return Updated decks
      */
-    virtual Result<void> update_decks(const QVector<Deck>& decks) = 0;
+    virtual Result<QVector<Deck>> update_decks(const QVector<Deck>& decks) = 0;
 
     /**
      * @brief Remove decks with the specified IDs
      * @param ids Vector of deck IDs to be deleted
+     * @return Deleted decks
      */
-    virtual Result<void> remove_decks(const QVector<Deck::id_type>& ids) = 0;
+    virtual Result<QVector<Deck>> remove_decks(const QVector<Deck::id_type>& ids) = 0;
 
     /**
      * @brief Fetch decks by specific IDs
      * @param ids Vector of deck IDs to fetch
      * @return QVector of fetched decks
      */
-    virtual Result<QVector<Deck>> fetch_decks(const QVector<Deck::id_type>& ids) = 0;
+    virtual Result<QVector<Deck>> fetch_decks(const QVector<Deck::id_type>& ids) const = 0;
 
     /**
      * @brief Fetch all decks
      * @return QVector of decks
      */
-    virtual Result<QVector<Deck>> fetch_decks() = 0;
+    virtual Result<QVector<Deck>> fetch_decks() const = 0;
 
     /**
      * @brief Update cards from a vector
      * @param cards Vector of card objects (`core::Card`) to be updated
+     * @return Updated cards
      */
-    virtual Result<void> update_cards(const QVector<Card>& cards) = 0;
+    virtual Result<QVector<Card>> update_cards(const QVector<Card>& cards) = 0;
 
     /**
      * @brief Create new cards from a vector
      * @param cards Vector of card objects (`core::Card`) to be created
+     * @return Created cards
      */
-    virtual Result<void> create_cards(const QVector<Card>& cards) = 0;
+    virtual Result<QVector<Card>> create_cards(const QVector<Card>& cards) = 0;
 
     /**
      * @brief Fetch all cards from a specific deck
      * @param deck_id ID of the deck
      * @return QVector of cards
      */
-    virtual Result<QVector<Card>> fetch_cards(Deck::id_type deck_id) = 0;
+    virtual Result<QVector<Card>> fetch_cards(Deck::id_type deck_id) const = 0;
 
     /**
      * @brief Fetch cards by specific IDs
      * @param ids Vector of card IDs to fetch
      * @return QVector of cards
      */
-    virtual Result<QVector<Card>> fetch_cards(const QVector<Card::id_type>& ids) = 0;
+    virtual Result<QVector<Card>> fetch_cards(const QVector<Card::id_type>& ids) const = 0;
 
     /**
      * @brief Fetch all cards
      * @return QVector of cards
      */
-    virtual Result<QVector<Card>> fetch_cards() = 0;
+    virtual Result<QVector<Card>> fetch_cards() const = 0;
 
     /**
      * @brief Remove cards with the specified IDs
      * @param ids Vector of card IDs to remove
+     * @return Removed cards
      */
-    virtual Result<void> remove_cards(const QVector<Card::id_type>& ids) = 0;
+    virtual Result<QVector<Card>> remove_cards(const QVector<Card::id_type>& ids) = 0;
 };
 
 } // namespace core
