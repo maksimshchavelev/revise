37439591637e7cefe848e1689fd2fe53e6981cdf
Maksim <maksimshchavelev@gmail.com>
2026-04-24 15:57:12 +0300

    refactor IDeckService

--- modules/core/include/core/service/IDeckService.hpp
+++ modules/core/include/core/service/IDeckService.hpp
@@ -2,142 +2,204 @@
 
 #pragma once
 
-#include "Card.hpp"                 // for Card
-#include "Deck.hpp"                 // for Deck
-#include "Notifiable.hpp"           // for Notifiable
-#include "search/ISearchEngine.hpp" // for search engine
-#include <expected>                 // for std::expected
+#include "core/Card.hpp"                    // for Card
+#include "core/Deck.hpp"                    // for Deck
+#include "core/Notifiable.hpp"              // for Notifiable
+#include "core/search/CardSearchFilter.hpp" // for CardFilterChain
+#include <QFuture>                          // for QFuture
+#include <expected>                         // for std::expected
 
 namespace core {
 
 /**
  * @brief Orchestrates all actions related to the deck and cards
- *
- * This class is useful if you want to delete, add, edit decks/cards, as well as import, export, and study them.
+ * @details This class is useful if you want to delete, add, edit decks/cards, as well as import and export them
  */
 class IDeckService : public Notifiable {
   public:
-    EVENT(deck_created)                 ///< Deck created
-    EVENT(deck_removed)                 ///< Deck removed
-    EVENT(decks_updated)                ///< Any deck has been updated
-    EVENT(deck_updated)                 ///< Deck updated
-    EVENT(import_started)               ///< Import started
-    EVENT(import_finished)              ///< Import finished
-    EVENT(deck_imported)                ///< Deck imported
-    EVENT(export_started)               ///< Export started
-    EVENT(export_finished)              ///< Export finished
-    EVENT(deck_exported)                ///< Deck exported
-    EVENT(card_created)                 ///< Card created
-    EVENT(cards_updated)                ///< Any card has been updated
-    EVENT(card_removed)                 ///< Card removed
+    EVENT(deck_created, Deck deck)      ///< Deck created. Stores created deck
+    EVENT(deck_removed, Deck deck)      ///< Deck removed. Stores removed deck
+    EVENT(deck_updated, Deck deck)      ///< Deck updated
+    EVENT(import_finished)              ///< Import finished with success or failure
+    EVENT(deck_imported, Deck deck)     ///< Deck imported (upon successful import). Stores imported deck
+    EVENT(export_finished)              ///< Export finished with success or failure
+    EVENT(deck_exported, Deck deck)     ///< Deck exported (upon successful export). Stores exported deck
+    EVENT(card_created, Card card)      ///< Card created. Stores created card
+    EVENT(card_removed, Card card)      ///< Card removed. Stores removed card
     EVENT(import_failed, QString error) ///< Import failed
     EVENT(export_failed, QString error) ///< Export failed
+
+    /// @brief Stores IDeckService error
+    struct Error {
+        enum class Kind {
+            NotFound,          ///< Requested entity does not exist
+            AlreadyExists,     ///< Entity already exists
+            ValidationFailed,  ///< Input data is invalid
+            ImportFailed,      ///< Import operation failed
+            ExportFailed,      ///< Export operation failed
+            UnsupportedFormat, ///< Unsupported import/export format
+            AccessDenied,      ///< Operation is forbidden
+            Busy,              ///< Another operation is already running
+            InternalError      ///< Unexpected internal failure
+        };
+
+        Kind    kind{Kind::InternalError};
+        QString message;
+
+        static Error not_found(QString message) {
+            return Error{.kind = Kind::NotFound, .message = std::move(message)};
+        }
+
+        static Error already_exists(QString message) {
+            return Error{.kind = Kind::AlreadyExists, .message = std::move(message)};
+        }
+
+        static Error validation_failed(QString message) {
+            return Error{.kind = Kind::ValidationFailed, .message = std::move(message)};
+        }
+
+        static Error import_failed(QString message) {
+            return Error{.kind = Kind::ImportFailed, .message = std::move(message)};
+        }
+
+        static Error export_failed(QString message) {
+            return Error{.kind = Kind::ExportFailed, .message = std::move(message)};
+        }
+
+        static Error unsupported_format(QString message) {
+            return Error{.kind = Kind::UnsupportedFormat, .message = std::move(message)};
+        }
+
+        static Error access_denied(QString message) {
+            return Error{.kind = Kind::AccessDenied, .message = std::move(message)};
+        }
+
+        static Error busy(QString message) {
+            return Error{.kind = Kind::Busy, .message = std::move(message)};
+        }
+
+        static Error internal_error(QString message) {
+            return Error{.kind = Kind::InternalError, .message = std::move(message)};
+        }
+    };
+
+    template <typename T> using Result = std::expected<T, Error>;
+
+    /// @brief Used as a draft for the deck to be created or updated
+    struct DeckDraft {
+        QString name;
+        QString description;
+        int     new_limit{0};
+        int     review_limit{0};
+        int     incorrect_limit{0};
+        float   time_limit{0.0f};
+    };
+
+    /// @brief Used as a draft for the card to be created
+    struct CreateCardDraft {
+        QString front;
+        QString back;
+    };
+
+    /// @brief Used as a draft for the card to be updated
+    struct UpdateCardDraft : CreateCardDraft {
+        float difficulty{0.0f};
+    };
+
 
     virtual ~IDeckService() = default;
 
     /**
-     * @brief create_deck Create deck
-     * @param deck Deck
-     * @return `std::expected` with `void` in case of success or with an error description in case of failure
-     * @note If successful, the `deck_created` and `decks_updated` events are emitted.
+     * @brief Create deck
+     * @param deck Deck draft
+     * @note Emits `deck_created` event if success
      */
-    virtual std::expected<void, QString> create_deck(const Deck& deck) = 0;
+    virtual QFuture<Result<void>> create_deck(const DeckDraft& deck) = 0;
 
     /**
-     * @brief Remove deck
-     * @param deck_id ID of the deck
-     * @return `std::expected` with `void` in case of success or with an error description in case of failure
-     * @note If successful, the `deck_removed` and `decks_updated` events are emitted.
-     */
-    virtual std::expected<void, QString> remove_deck(int deck_id) = 0;
-
-    /**
-     * @brief Import deck asynchronously
-     * @param path Path to the deck
-     * @note When importing begins, the `import_started` event is emitted; when it ends, the `import_finished` event
-     * is emitted. If successful, the `deck_imported` and `decks_updated` events are emitted. In case of an error, the
-     * `import_failed` event is emitted.
-     */
-    virtual void import_deck_async(const QString& path) = 0;
-
-    /**
-     * @brief Exports the deck in `Revise` format (`rpkg`)
-     * @param deck_id ID of the deck to be exported
-     * @param path Where to save the result
-     * @note When exporting begins, the `export_started` event is emitted, and when it ends, the `export_finished`
-     * event is emitted. If successful, the `deck_exported` event is emitted. In case of an error, the `export_failed`
-     * event is emitted.
-     */
-    virtual void export_deck_async(int deck_id, const QString& path) = 0;
-
-    /**
-     * @brief Get information about the deck by id
+     * @brief Remove deck by ID
      * @param deck_id Deck ID
-     * @return `std::expected`, where `core::Deck` is located in case of success, and an error description in case of
-     * failure
+     * @note Emits `deck_removed` event if success
      */
-    virtual std::expected<Deck, QString> deck(int deck_id) = 0;
+    virtual QFuture<Result<void>> remove_deck(Deck::id_type deck_id) = 0;
+
+    /**
+     * @brief Import deck
+     * @param path Path to the deck
+     * @note Emits `deck_imported` if deck successfully imported. Emits `import_finished` in any case
+     */
+    virtual QFuture<Result<void>> import_deck(const QString& path) = 0;
+
+    /**
+     * @brief Export deck
+     * @param deck_id Deck id
+     * @param path Path to the deck
+     * @note Emits `deck_exported` if deck successfully exported. Emits `export_finished` in any case
+     */
+    virtual QFuture<Result<void>> export_deck(Deck::id_type deck_id, const QString& path) = 0;
+
+    /**
+     * @brief Fetch deck by ID
+     * @param deck_id Deck ID
+     * @return Fetched deck
+     */
+    virtual QFuture<Result<Deck>> deck(Deck::id_type deck_id) const = 0;
 
     /**
      * @brief Update deck
+     * @param deck_id Deck ID
      * @param deck Deck
-     * @return `std::expected` with `void` in case of success or with an error description in case of failure
-     * @note If successful, emits the `decks_updated` event.
+     * @note Emits `deck_updated` event if success
      */
-    virtual std::expected<void, QString> update_deck(const Deck& deck) = 0;
+    virtual QFuture<Result<void>> update_deck(Deck::id_type deck_id, const DeckDraft& deck) = 0;
 
     /**
      * @brief Create a card
      * @param card Card
-     * @return `std::expected` with `void` in case of success or with an error description in case of failure
-     * @note If successful, emits the `card_created` and `cards_updated` events.
+     * @note Emits `card_created` event if success
      */
-    virtual std::expected<void, QString> create_card(const Card& card) = 0;
+    virtual QFuture<Result<void>> create_card(const CreateCardDraft& card) = 0;
 
     /**
-     * @brief Get information about the card
+     * @brief Fetch card
      * @param id Card ID
-     * @return `std::expected` with `core::Card` in case of success or with an error description in case of failure
+     * @return Fetched card
      */
-    virtual std::expected<Card, QString> card(int id) = 0;
+    virtual QFuture<Result<Card>> card(Card::id_type id) const = 0;
 
     /**
      * @brief Remove card
      * @param id ID of the card
-     * @return `std::expected` with `core::Card` in case of success or with an error description in case of failure
-     * @note If successful, emits the `card_removed` and `cards_updated` events.
+     * @note Emits `card_removed` event if success
      */
-    virtual std::expected<void, QString> remove_card(int id) = 0;
+    virtual std::expected<void, QString> remove_card(Card::id_type id) = 0;
 
     /**
      * @brief Update card
      * @param card Card
-     * @return `std::expected` with `core::Card` in case of success or with an error description in case of failure
-     * @note If successful, emits the `cards_updated` event.
+     * @note Emits `card_updated` event if success
      */
-    virtual std::expected<void, QString> update_card(const Card& card) = 0;
+    virtual QFuture<Result<void>> update_card(const UpdateCardDraft& card) = 0;
 
     /**
-     * @brief deck_summaries Get information about each deck
-     * @return `std::expected` with `QVector<DeckSummary>` in case of success or with an error description in case of
-     * failure
+     * @brief Get information about each deck
+     * @return QVector of deck summary
      */
-    virtual std::expected<QVector<DeckSummary>, QString> deck_summaries() = 0;
+    virtual QFuture<Result<QVector<DeckSummary>>> deck_summaries() const = 0;
 
     /**
-     * @brief Get a list of all cards
+     * @brief Get a list of all cards in specified deck
      * @param deck_id Deck ID
-     * @return `std::expected` with `QVector<Card>` in case of success or with an error description in case of
-     * failure
+     * @return QVector of cards
      */
-    virtual std::expected<QVector<Card>, QString> cards(int deck_id) = 0;
+    virtual QFuture<Result<QVector<Card>>> cards(Deck::id_type deck_id) const = 0;
 
     /**
      * @brief Search cards in the database
      * @param filters Search filters
-     * @return `std::expected<QVector<Card>, SearchError>`
+     * @return QVector of cards
      */
-    virtual std::expected<QVector<Card>, SearchError> search_cards(const CardFilterChain& filters) = 0;
+    virtual QFuture<Result<QVector<Card>>> search_cards(const CardFilterChain& filters) const = 0;
 };
 
 } // namespace core
